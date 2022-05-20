0cebd8c9517ca4e3518a1ee24d5b5f90ef580d29
caesarkutaa <caesarlouis0@gmail.com>
2022-05-20 13:03:36 +0100

    added src

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+<section class="flex w-full">
+    <div class="m-auto">
+        <h1>Caculator</h1>
+    </div>
+</section>
+   
+
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
