ae9ab7c236d37f2c8c504ec0e0799d660e05eb7e
caesarkutaa <caesarlouis0@gmail.com>
2022-05-21 21:57:23 +0100

    added appvue

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,19 @@
 <template>
-<div class="w-full flex">
-    <div class="m-auto">
-    
-  <h1>Available heros: {{heroscount }}</h1>
+<section> <h1></h1>
+
+
+
+
+</section>
   
-  <ul>
-    <li v-for="(hero,index) in dcheros " :key="index">
-    
-    <div>{{ hero.name }} <button @click="remove(index)">x</button></div>
-    </li>  
-  </ul>
-  <form @submit.prevent="addheros">
-  <input  v-model ="newhero" placeholder="Type hero name here"/>  
- <button type="submit">Add Hero</button>
-</form>
-</div>
-</div>
 </template>
 
 
 
-<script>   
-export default {  
-    computed:{
-        heroscount(){
-            return this.dcheros.length;
-        },
-       
-    }, 
-   
+<script>
+export default {
+    data
 
-    methods:{
-        addheros(){
-            if(this.newhero !==""){
-            this.dcheros.push( {name: this.newhero }); this.newhero = '';
-            }
-            
-        },      
-         remove(index){
-      this.dcheros = this.dcheros.filter((hero,i) => i != index)
-
-
-    },
-    },
-    data(){
-        return{
-          
-            newhero:"", 
-            dcheros:[
-             
-            ]
-
-        }
-        
-    }
-
-    
- 
 }
 </script>
 
