cdc64149079c5f7fa079662770ebd352736ab5ff
caesarkutaa <caesarlouis0@gmail.com>
2022-05-27 16:44:25 +0100

    added styles and codes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,72 @@
 <template>
-  
+  <p>
+      {{ days }} days,
+    {{ hours }} hours, {{ minutes }} minute, {{ seconds }} seconds
+    
+    
+  </p>
 </template>
 
 <script>
+
 export default {
+  name: "App",
+  data() {
+    return {
+        endate: new Date("Jun 28, 2022 10:00:00").getTime(),
+        interval:"",
+        days:"",
+        hours:"",
+        minutes:"",
+        seconds:"",
+    };
+  },
+  methods:{
+        /**
+         * Method to get the number of days to countdown
+         */
+        getDays(){
+            return Math.floor(this.interval / (1000 * 60 * 60 * 24))
+        },
+     
+        /**
+         * Method to get the number of hour to countdown
+         */
+        gethours(){
+             return Math.floor((this.interval % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        },
+        
+        
+        /**
+         * Method to get the number of days to countdown
+         */
+        getMinutes(){
+            return Math.floor((this.interval % (1000 * 60 * 60  )) / (1000 * 60))
+        },
 
+       
+        /**
+         * Method to get the number of days to countdown
+         */
+        getseconds(){
+            return Math.floor((this.interval % (1000 * 60 )) / (1000))
+        },
+
+       startCountdown() {
+            this.funcInterval = setInterval(() => {
+                let dateTo = new Date().getTime()
+                this.interval = this.endate - dateTo
+                this.days = this.getDays()
+                this.hours = this.gethours()
+                this.minutes = this.getMinutes()
+                this.seconds = this.getseconds()
+
+       })
+  },
+},
+ mounted () {
+        this.startCountdown();
+    },
 }
+
 </script>
-
-<style>
-
-</style>
